506c85fd6a77a1c1aff23d8958e9aa7cb9df79c6
Prashant <31570203+prashant-raghu@users.noreply.github.com>
2018-05-09 18:51:44 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,5 @@
 export const environment = {
   production: true,
-    // Initialize Firebase
-  firebase: {
-    apiKey: 'AIzaSyCzRIqx70Ao60liRmEX3LfbBYHCll-oKmk',
-    authDomain: 'unvicenews.firebaseapp.com',
-    databaseURL: 'https://unvicenews.firebaseio.com',
-    projectId: 'unvicenews',
-    storageBucket: 'unvicenews.appspot.com',
-    messagingSenderId: '456941829925'
-  },
+
  
 };
